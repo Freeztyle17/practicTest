8cbe81cf49003be9710231e9a4e06beed202dba0
Freeztyle17 <fedorov.alexey2018@yandex.ru>
2024-03-29 12:50:31 +0400

    aaa2

--- src/test/java/ru/fedotov/SpringWebMVC/SpringWebMvcApplicationTests.java
+++ src/test/java/ru/fedotov/SpringWebMVC/SpringWebMvcApplicationTests.java
@@ -3,11 +3,15 @@ package ru.fedotov.SpringWebMVC;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class SpringWebMvcApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String bbbbb = "BBBB";
+		assertEquals(bbbbb, bbbbb);
 	}
 
 }
